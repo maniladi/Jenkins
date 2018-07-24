8abc0110d5821bdaaa58e222186751b218a4dcb4
maniladi <manikantaladi15@gmail.com>
2018-07-24 09:01:35 +0530

    bac.java

---
+++ bac.java
@@ -0,0 +1,14 @@
+class bac 
+{
+	public static void main(String[] args) 
+	{
+		int a,b,c;
+		a=50;
+		c=200;
+		b=201;
+		c=a+b+c;
+System.out.println("The sum of three values are "+c);
+
+		int a=500,b=333,c=222,c=a+b+c;
+		System.out.println("The sum of three values are "+c=a+b+c);
+
